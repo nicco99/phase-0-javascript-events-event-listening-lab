8eef76a06cf778a419d4c56b567b0b4aa50f5e39
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-08-17 17:44:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-function addingEventListener() {
-}
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', clickAlert);
+
+  }
+    
+
+addingEventListener();
